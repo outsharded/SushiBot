c63b609e611707e7b2dfbbf226a8e6577ea64fb3
tecdude <92227828+tecdude@users.noreply.github.com>
2023-02-15 15:10:53 +0000

    init

---
+++ commands/testping.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { colour } = require("../settings.json");
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Check if the bot is online'),
+	async execute(interaction) {
+		//define first response
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		//send response
+		sent
+		//then update message with ping
+		const pinged = new EmbedBuilder()
+			.setColor(colour)
+			.setTitle(`Pong! :ping_pong:`)
+			.addFields(
+				{ name: 'Roundtrip latency', value: `${sent.createdTimestamp - interaction.createdTimestamp}` + ' ms' },
+			)
+			.setTimestamp()
+		interaction.editReply({ content: '', embeds: [pinged] });
+		console.log('Ping command - completed')
+	},
+};
